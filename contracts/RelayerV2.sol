efe46a2d481b1afab9a242f7a64abd4f03103bd0
33potolok <142433745+33potolok@users.noreply.github.com>
2023-11-19 14:15:50 +0100

    Update RelayerV2.sol

--- contracts/RelayerV2.sol
+++ contracts/RelayerV2.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity ^0.7.6;
+pragma solidity ^0.7.7;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
